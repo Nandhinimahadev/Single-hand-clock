21f2618375d1c8f048592beb72943d57af427c15
Nandhini <nandhini.m@excelenciaconsulting.com>
2024-06-15 19:58:11 +0530

    fixes

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ function updateClock() {
     const minutes = now.getMinutes();
     console.log("now",now)
 
-    const degrees = ((hours % 12) + minutes / 60) * 30+60;
+    const degrees = ((hours % 12) + minutes / 60) * 30+55;
     console.log("degrees",degrees)
     const clock = document.querySelector('.numbers');
     clock.style.transform = `rotate(-${degrees}deg)`; 
+    
+    const timeDisplay = document.querySelector('.time-display');
+    const formattedTime = now.toLocaleTimeString('en-US');
+    timeDisplay.textContent = formattedTime;
   }
   
   setInterval(updateClock, 1000);
